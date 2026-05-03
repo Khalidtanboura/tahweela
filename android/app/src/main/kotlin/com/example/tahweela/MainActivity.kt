a670b2fba1dbcfac52107cda8b9f06a7a9b63786
Khalid Anan Mohammed Tanboura <khalidtanboura@gmail.com>
2026-05-03 11:02:29 +0300

    initial commit

---
+++ android/app/src/main/kotlin/com/example/tahweela/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tahweela
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tahweela app',
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+
+        title: Text('app'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: .center,
+          children: [
+            Text('tahweela', style: Theme.of(context).textTheme.headlineMedium),
+          ],
+        ),
+      ),
+    );
+  }
+}
